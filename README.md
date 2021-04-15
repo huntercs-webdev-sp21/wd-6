0509973d1cb8575c8756363c01aa27491c023682
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-04-15 18:59:49 -0400

    base for hw

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Shakil Rafi, Jasper Cheung
 Project Link: 
 
 Assigment Link: https://docs.google.com/document/d/1bjXuk0TnSevHghRr6aIkwreF1HDrKDv4W5_PY_P2IVY/edit
+
+API Link: [https://getpantry.cloud/](https://getpantry.cloud/)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+const BASE_URL = 'https://getpantry.cloud/apiv1/pantry/ecf03d36-db58-4d1f-b2d2-be3de4515084/';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="header">Secrets Locker</h1>
+      {/* read */}
+      <h2 className="header">Get data:</h2>
+
+      {/* create/update/delete */}
+      <h2 className="header">Manage data:</h2>
     </div>
   );
 }
