f52daae9f110b1358ceb076a8450791ad000d31b
Shakil Rafi <19642235+srafi1@users.noreply.github.com>
2021-04-15 21:52:58 -0400

    edit a message

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import './App.css';
 // API documentation: https://documenter.getpostman.com/view/3281832/SzmZeMLC
 const BASE_URL = 'https://getpantry.cloud/apiv1/pantry/ecf03d36-db58-4d1f-b2d2-be3de4515084/basket/';
 
-const randomChar = () => String.fromCharCode(Math.floor(97 + Math.random()*26));
+const randomChar = () => String.fromCharCode(Math.floor(97 + Math.random() * 26));
 
 function App() {
   const [body, setBody] = useState('');
+  const [basketIndex, setBasketIndex] = useState(-1);
   const [baskets, setBaskets] = useState([]);
 
   const createBasket = () => {
     // four random chars as name
     const basketName = randomChar() + randomChar() + randomChar() + randomChar();
-    const basket = {name: basketName, body: body};
-    axios.post(BASE_URL + basketName, basket.body)
+    const basket = { name: basketName, body: body };
+    axios.post(BASE_URL + basketName, basket)
       .then(() => setBaskets([...baskets, basket]))
-      .catch(() => console.log('failure'));
+      .catch(() => console.log('create failure'));
+  }
+
+  const updateBasket = () => {
+    const basket = {name: baskets[basketIndex].name, body: body};
+    axios.put(BASE_URL + baskets[basketIndex].name, basket)
+      .then(() => {
+        baskets[basketIndex].body = body;
+        setBaskets([...baskets]);
+      })
+      .catch(() => console.log('edit failure'));
+  }
+
+  const onSave = () => {
+    if (basketIndex < 0) {
+      createBasket();
+    } else {
+      updateBasket();
+    }
   }
 
   return (
@@ -28,15 +47,18 @@ function App() {
 
       {/* create/update/delete */}
       <h2 className="header">Manage data:</h2>
-      <h3 className="header">Create New:</h3>
+      <h3 className="header">
+        {basketIndex < 0 ? 'Create New:' : `Edit basket '${baskets[basketIndex].name}':`}
+      </h3>
       <textarea placeholder="Data" onChange={e => setBody(e.target.value)} />
-      <button onClick={createBasket}>Create</button>
+      <button onClick={onSave}>Save</button>
 
       <h3 className="header">Update/Delete:</h3>
-      {baskets.map(basket => (
-        <div className="basket container">
+      {baskets.map((basket, i) => (
+        <div key={basket.name} className="basket container">
           <h4>{basket.name}</h4>
           <p>{basket.body}</p>
+          <button onClick={() => setBasketIndex(i)}>Edit</button>
         </div>
       ))}
     </div>
